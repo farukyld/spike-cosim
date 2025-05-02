800a680d775cd76a418a59db617a75c0ade87650
farukyld <123uzunince123@gmail.com>
2025-05-02 14:57:27 +0300

    remove old code

--- cosim-io/src/receiver.cc
+++ cosim-io/src/receiver.cc
@@ -63,30 +63,7 @@ int main()
   {
     if (!simulation_completed())
     {
-      // for (size_t i = 0; i < 100; i++)
       step(); // spike simulasyon step
-      const int MAX_COMMIT_LOG_LENGTH = 200;
-      uint8_t generated_buffer[MAX_COMMIT_LOG_LENGTH];
-      uint8_t received_buffer[MAX_COMMIT_LOG_LENGTH];
-      auto state = s_ptr->get_core(0)->get_state();
-      size_t bytes_written = pack_commit_log_into_array(generated_buffer, MAX_COMMIT_LOG_LENGTH, *state);
-
-      // reading bytes_written bytes.
-      // assuming other end of the simulation
-      // writes equal number of bytes for the current step
-      // receive it and compare with the
-      int recv_status = recv(client_sock_fd, &received_buffer, bytes_written, 0);
-      if (recv_status < 0)
-      {
-        perror("Receive failed");
-        close(client_sock_fd);
-        close(server_sock_fd);
-        return 1;
-      }
-      if (memcmp(received_buffer, generated_buffer, bytes_written) != 0)
-      {
-        // fprintf(stderr, "buffers dosern't match\n");
-      }
     }
     else
     {
