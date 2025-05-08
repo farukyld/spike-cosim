934b6207044e5deceaca2b56153d40a757313d36
farukyld <123uzunince123@gmail.com>
2025-05-08 12:48:25 +0300

    verilator cosim düzeltildi

--- cosim-common/cosimif.h
+++ cosim-common/cosimif.h
@@ -4,8 +4,13 @@
 #include "sim.h"
 
 extern sim_t* s_ptr;
+#ifdef VERILATOR
+#include "cosim_ornek_kullanim__Dpi.h"
+#else
 void init();
 void step();
 uint8_t simulation_completed();
+#endif
+
 
 #endif
