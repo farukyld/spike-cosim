8299e941e35626d1774cb5a3e260c6b5ac6ab84f
farukyld <123uzunince123@gmail.com>
2025-06-24 11:32:29 +0300

    testbench'te print'ler comment'lendi. daha hızlı çalıştırıp sonucu görmek için

--- cosim-verilator/src/tb/cosim_ornek_kullanim.sv
+++ cosim-verilator/src/tb/cosim_ornek_kullanim.sv
@@ -12,8 +12,8 @@ module cosim_ornek_kullanim;
     init();
 
     for (;;) begin: simulation_loop
-      get_pc(temp_pc);
-      $display("pc: %0h", temp_pc);
+      // get_pc(temp_pc);
+      // $display("pc: %0h", temp_pc);
       if (simulation_completed()) begin // htif_t::exitcode != 0
         $display("simulation completed");
         $finish;
@@ -23,41 +23,41 @@ module cosim_ornek_kullanim;
 
       step();
 
-      get_log_reg_write(log_reg_write_from_c, num_elements_inserted_from_c_side);
+      // get_log_reg_write(log_reg_write_from_c, num_elements_inserted_from_c_side);
 
-      for (int ii = 0; ii < num_elements_inserted_from_c_side; ii = ii + 1) begin: log_reg_write_itr
+      // for (int ii = 0; ii < num_elements_inserted_from_c_side; ii = ii + 1) begin: log_reg_write_itr
 
-        $display("log_reg_write_from_c[%0d] reg_type: %0s", 
-        ii, log_reg_write_from_c[ii].key.reg_type.name);
+      //   $display("log_reg_write_from_c[%0d] reg_type: %0s", 
+      //   ii, log_reg_write_from_c[ii].key.reg_type.name);
 
-        if (log_reg_write_from_c[ii].key.reg_type == CSR) begin
-          $display("log_reg_write_from_c[%0d] csr name: %0s",
-          ii, log_reg_write_from_c[ii].key.reg_id.csr_id.name);
-        end else begin
-          $display("log_reg_write_from_c[%0d] reg_id: %0d", 
-          ii, log_reg_write_from_c[ii].key.reg_id);
-        end
+      //   if (log_reg_write_from_c[ii].key.reg_type == CSR) begin
+      //     $display("log_reg_write_from_c[%0d] csr name: %0s",
+      //     ii, log_reg_write_from_c[ii].key.reg_id.csr_id.name);
+      //   end else begin
+      //     $display("log_reg_write_from_c[%0d] reg_id: %0d", 
+      //     ii, log_reg_write_from_c[ii].key.reg_id);
+      //   end
 
-        $display("log_reg_write_from_c[%0d].value: %0h", ii, log_reg_write_from_c[ii].value);
-      end
+      //   $display("log_reg_write_from_c[%0d].value: %0h", ii, log_reg_write_from_c[ii].value);
+      // end
 
-      get_log_mem_read(log_mem_read_from_c, num_elements_inserted_from_c_side);
+      // get_log_mem_read(log_mem_read_from_c, num_elements_inserted_from_c_side);
 
-      for (int ii = 0; ii < num_elements_inserted_from_c_side; ii = ii + 1) begin: log_mem_read_itr
-        $display("log_mem_read_from_c[%0d].addr: %0h", ii, log_mem_read_from_c[ii].addr);
-        $display("log_mem_read_from_c[%0d].paddr: %0h", ii, log_mem_read_from_c[ii].paddr);
-        $display("log_mem_read_from_c[%0d].wdata: %0h", ii, log_mem_read_from_c[ii].wdata);
-        $display("log_mem_read_from_c[%0d].len: %0d", ii, log_mem_read_from_c[ii].len);
-      end
+      // for (int ii = 0; ii < num_elements_inserted_from_c_side; ii = ii + 1) begin: log_mem_read_itr
+      //   $display("log_mem_read_from_c[%0d].addr: %0h", ii, log_mem_read_from_c[ii].addr);
+      //   $display("log_mem_read_from_c[%0d].paddr: %0h", ii, log_mem_read_from_c[ii].paddr);
+      //   $display("log_mem_read_from_c[%0d].wdata: %0h", ii, log_mem_read_from_c[ii].wdata);
+      //   $display("log_mem_read_from_c[%0d].len: %0d", ii, log_mem_read_from_c[ii].len);
+      // end
 
-      get_log_mem_write(log_mem_write_from_c, num_elements_inserted_from_c_side);
+      // get_log_mem_write(log_mem_write_from_c, num_elements_inserted_from_c_side);
 
-      for (int ii = 0; ii < num_elements_inserted_from_c_side; ii = ii + 1) begin: log_mem_write_itr
-        $display("log_mem_write_from_c[%0d].addr: %0h", ii, log_mem_write_from_c[ii].addr);
-        $display("log_mem_write_from_c[%0d].paddr: %0h", ii, log_mem_write_from_c[ii].paddr);
-        $display("log_mem_write_from_c[%0d].wdata: %0h", ii, log_mem_write_from_c[ii].wdata);
-        $display("log_mem_write_from_c[%0d].len: %0h", ii, log_mem_write_from_c[ii].len);
-      end
+      // for (int ii = 0; ii < num_elements_inserted_from_c_side; ii = ii + 1) begin: log_mem_write_itr
+      //   $display("log_mem_write_from_c[%0d].addr: %0h", ii, log_mem_write_from_c[ii].addr);
+      //   $display("log_mem_write_from_c[%0d].paddr: %0h", ii, log_mem_write_from_c[ii].paddr);
+      //   $display("log_mem_write_from_c[%0d].wdata: %0h", ii, log_mem_write_from_c[ii].wdata);
+      //   $display("log_mem_write_from_c[%0d].len: %0h", ii, log_mem_write_from_c[ii].len);
+      // end
       // wait_key();
 
     end: simulation_loop
